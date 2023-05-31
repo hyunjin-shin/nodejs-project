9cbad9ffd37b8ebad4ec7099ebc4c80bb7035014
Jin <jin@Jinui-MacBookAir.local>
2023-05-31 04:44:23 -0400

    Login Front update

--- app/app.js
+++ app/app.js
@@ -12,11 +12,11 @@ const home = require("./src/routes/home");
 //App setting
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-app.use(express.static(`${__dirname}/src/public`))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(`${__dirname}/src/public`)) // what is this for exactly?
+app.use(bodyParser.json()); // what is this for exactly?
+app.use(bodyParser.urlencoded({extended: true})); // what is this for exactly?
 
-app.use("/", home); //middleWare
+app.use("/", home); //middleWare - find the matching request in home directory
 
 module.exports = app;
 
